dbb5fe560ae2b539479ba69103176ce5fa5d8a11
Pilzinsel64 <pilzinsel64@gmx.de>
2023-07-21 06:42:34 +0200

    nextcloud: enable theming by default
    Fix #2456

--- src/nextcloud/fixes/fresh-install/1_disable-theming.sh
+++ src/nextcloud/fixes/fresh-install/1_disable-theming.sh
@@ -1,10 +1,12 @@
 #!/bin/sh -e
 
 # shellcheck source=src/common/utilities/common-utilities
-. "$SNAP/utilities/common-utilities"
+#. "$SNAP/utilities/common-utilities"
 
 # Disable the theming app. It requires imagick (which the snap doesn't ship) and
 # displays a warning if it's not installed. This way, the warning is only shown if
 # someone needs and enables the theming app.
-run_command "Disabling theming by default" occ -n app:disable theming
+#run_command "Disabling theming by default" occ -n app:disable theming
 
+# NOTE: This script has been disabled. The theming app is a hard dependency since NC25
+# and it is no longer possible to disable on a common way.
